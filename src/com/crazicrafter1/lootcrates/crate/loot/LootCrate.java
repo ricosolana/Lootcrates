8d5906475b6c436743b23ce04f361783b619ae56
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2021-04-05 12:09:51 -0400

    rewarded crates are now seasonal

--- src/com/crazicrafter1/lootcrates/crate/loot/LootCrate.java
+++ src/com/crazicrafter1/lootcrates/crate/loot/LootCrate.java
@@ -1,10 +1,23 @@
 package com.crazicrafter1.lootcrates.crate.loot;
 
+import com.crazicrafter1.lootcrates.crate.ActiveCrate;
 import com.crazicrafter1.lootcrates.crate.Crate;
+import com.crazicrafter1.lootcrates.util.Util;
+import org.bukkit.inventory.ItemStack;
 
 public class LootCrate extends LootItem {
 
+    private Crate crate;
+
     public LootCrate(Crate crate, int min, int max) {
-        super(crate.getItemStack(1), min, max);
+        //super(crate.getItemStack(1), min, max);
+        super(null, min, max);
+        this.crate = crate;
+    }
+
+    @Override
+    public ItemStack getAccurateVisual() {
+        // just return the crate item (since seasonal can change it)
+        return crate.getItemStack(Util.randomRange(min, max));
     }
 }

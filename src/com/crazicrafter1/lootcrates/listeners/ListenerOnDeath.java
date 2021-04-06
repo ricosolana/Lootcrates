ce8f30424cfb5f1e62a1934c8e595299dde23c5b
crazicrafter1 <47781580+crazicrafter1@users.noreply.github.com>
2021-04-06 09:10:03 -0400

    version

--- src/com/crazicrafter1/lootcrates/listeners/ListenerOnDeath.java
+++ src/com/crazicrafter1/lootcrates/listeners/ListenerOnDeath.java
@@ -2,10 +2,17 @@ package com.crazicrafter1.lootcrates.listeners;
 
 import com.crazicrafter1.lootcrates.Main;
 import com.crazicrafter1.lootcrates.crate.Crate;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+
+import java.util.*;
 
 public class ListenerOnDeath extends BaseListener {
 
@@ -20,5 +27,7 @@ public class ListenerOnDeath extends BaseListener {
             Crate.closeCrate(p);
         }
     }
+
+
 
 }
